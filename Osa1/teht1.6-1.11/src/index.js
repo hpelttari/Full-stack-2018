ae9d0595ce41fb4591bb4ec0efa111f7fc4505e6
hpelttari <hannu.pelttari@gmail.com>
2018-07-12 18:57:30 +0300

    Tehtävä 1.7 positiivisten lisäys (eli nyt oikeasti valmis)

--- Osa1/teht1.6-1.11/src/index.js
+++ Osa1/teht1.6-1.11/src/index.js
@@ -10,7 +10,8 @@ class App extends React.Component {
         hyva: 0,
         neutraali: 0,
         huono: 0,
-        ka: 0
+        ka: 0,
+        positiivisia: 0
       }
     }
 
@@ -20,16 +21,19 @@ class App extends React.Component {
         const hyva = (newValue) => () => { 
             this.setState({ hyva: newValue })
             this.setState({ka: (newValue-this.state.huono)/(newValue+this.state.neutraali+this.state.huono)})
+            this.setState({positiivisia: (newValue/(newValue+this.state.neutraali+this.state.huono))*100})
           }
 
           const huono = (newValue) => () => { 
             this.setState({ huono: newValue })
             this.setState({ka: (this.state.hyva-newValue)/(this.state.hyva+this.state.neutraali+newValue)})
+            this.setState({positiivisia: (this.state.hyva/(this.state.hyva+this.state.neutraali+newValue))*100})
           }
 
           const neutraali = (newValue) => () => { 
             this.setState({ neutraali: newValue })
             this.setState({ka: (this.state.hyva-this.state.huono)/(this.state.hyva+newValue+this.state.huono)})
+            this.setState({positiivisia: (this.state.hyva/(newValue+this.state.hyva+this.state.huono))*100})
           }
 
       return (
@@ -43,6 +47,7 @@ class App extends React.Component {
           <p>neutraali {this.state.neutraali}</p>
           <p>huono {this.state.huono}</p>
           <p>keski-arvo {this.state.ka}</p>
+          <p>positiivisia {this.state.positiivisia} %</p>
         </div>
       )
     }
